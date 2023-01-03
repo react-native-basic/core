c5e5372fb9fe3416a217db6e15da503ad93c442a
Sugih Ilmi Kalih Putra <sugih@salamenterprise.com>
2023-01-03 11:23:01 +0700

    refactor(component): export all components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-import BasicProvider from './components/context/BasicProvider';
+//Provider
+import BasicProvider, { useBasic } from './components/context/BasicProvider';
 
-export { BasicProvider };
+//Form
+import CheckBox from './components/form/CheckBox';
+import PasswordInput from './components/form/PasswordInput';
+import RadioButton from './components/form/RadioButton';
+import TextInput from './components/form/TextInput';
+
+//Regular Component
+import AppBar from './components/AppBar';
+import Box from './components/Box';
+import Divider from './components/Divider';
+import ListMenu from './components/ListMenu';
+import Modal from './components/Modal';
+import Row from './components/Row';
+import Screen from './components/Screen';
+import Toast from './components/Toast';
+import Typography from './components/Typography';
+
+export {
+    BasicProvider,
+    useBasic,
+    CheckBox,
+    RadioButton,
+    TextInput,
+    PasswordInput,
+    AppBar,
+    Box,
+    Divider,
+    ListMenu,
+    Modal,
+    Row,
+    Screen,
+    Toast,
+    Typography,
+};
