aac52e8ed38b8237e77b6cb62ae1c2e6e0d9f4ef
Sugih Ilmi Kalih Putra <sugih@salamenterprise.com>
2023-01-03 13:20:13 +0700

    refactor(BasicProvider): add darkMode props

--- src/components/context/BasicProvider.tsx
+++ src/components/context/BasicProvider.tsx
@@ -1,15 +1,22 @@
 import React, { createContext, useContext } from 'react';
-import { defaultConfig } from './defaultConfig';
+import { darkColors, defaultConfig, lightColors } from './defaultConfig';
 
 const BasicContext = createContext(defaultConfig);
 
 interface TypeProps {
     children: React.ReactNode;
+    darkMode?: boolean;
 }
 
-const BasicProvider: React.FC<TypeProps> = ({ children }) => {
+const BasicProvider: React.FC<TypeProps> = ({ children, darkMode = false }) => {
     return (
-        <BasicContext.Provider value={defaultConfig}>
+        <BasicContext.Provider
+            value={{
+                ...defaultConfig,
+                darkMode: darkMode,
+                colors: darkMode ? darkColors : lightColors,
+            }}
+        >
             {children}
         </BasicContext.Provider>
     );
